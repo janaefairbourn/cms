15f7d78c646f4237062ad514595778760baf6eae
janaefairbourn <fairbourn.janae@gmail.com>
2019-03-22 10:40:04 -0600

    Week 4 Complete

--- src/app/documents/document-list/document-list.component.ts
+++ src/app/documents/document-list/document-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Document} from '../document.model';
 
 @Component({
   selector: 'cms-document-list',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./document-list.component.css']
 })
 export class DocumentListComponent implements OnInit {
+  @Output() selectedDocumentEvent = new EventEmitter<Document>();
+  documents: Document[] = [
+    new Document('CIT 260', 'Object Oriented Programming', 'This is a test1', 'test.com'),
+    new Document('CIT 366', 'Full Web Stack Development', 'This is a test2', 'test.com'),
+    new Document('CIT 425', 'Data Warhousing', 'This is a test3', 'test.com'),
+    new Document('Cit 460', 'Enterprise Development', 'This is a test4', 'test.com')
+  ]
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSelectedDocument (document: Document) {
+    this.selectedDocumentEvent.emit(document);
   }
 
 }
